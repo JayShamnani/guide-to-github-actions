ee28f8858fc3dd7b70016a380d659f503f375d5f
shamnani.jay@gmail.com <shamnani.jay@gmail.com>
2021-09-09 16:53:12 +0530

    commiting to another branch

--- actions/entrypoint.bash
+++ actions/entrypoint.bash
@@ -1,5 +1,25 @@
 #!/bin/bash
 
+apt update && apt install git -y
+
+git config --global user.name "$USER_NAME"
+git config --global user.email "$USER_EMAIL"
+
+cd $GITHUB_WORKSPACE
+
+touch "example.txt"
+
+
+echo "You are doing awesome !" > example.txt
+
+git checkout -b $BRANCH_NAME
+
+git add .
+
+git commit -m "$COMMIT_MESSAGE"
+
+git push origin $BRANCH_NAME
+
 echo "Hey There"
 
 echo "This script is made possible by $deployment_env"
